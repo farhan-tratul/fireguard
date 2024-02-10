1da8021ab80ff058dd5dfe9a259eeb59a4bfe642
Arifin009 <arifinzaman1010@gmail.com>
2024-02-11 00:47:10 +0600

    homepage complete

--- app/src/main/java/edu/ewubd/fireguard/ui/home/HomeFragment.java
+++ app/src/main/java/edu/ewubd/fireguard/ui/home/HomeFragment.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
