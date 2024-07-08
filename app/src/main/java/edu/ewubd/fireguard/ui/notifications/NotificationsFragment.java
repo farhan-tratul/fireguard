e95baa8d08c7b91a5abe73abb5f6b3fb3a47102e
Arifin <aririfinzaman1010@gmail.com>
2024-07-08 16:02:48 +0600

    fire notification complete and alarm is done

--- app/src/main/java/edu/ewubd/fireguard/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/ewubd/fireguard/ui/notifications/NotificationsFragment.java
@@ -1,6 +1,12 @@
 package edu.ewubd.fireguard.ui.notifications;
 
+import static edu.ewubd.fireguard.ui.NotificationDatabaseHelper.TABLE_NOTIFICATIONS;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,17 +21,23 @@ import java.util.List;
 
 import edu.ewubd.fireguard.R;
 import edu.ewubd.fireguard.databinding.FragmentNotificationsBinding;
+import edu.ewubd.fireguard.ui.NotificationDatabaseHelper;
 
 public class NotificationsFragment extends Fragment {
-
     private FragmentNotificationsBinding binding;
     private RecyclerView recyclerView;
     private NotificationAdapter adapter;
     private List<Notification> notificationList;
+    private NotificationDatabaseHelper dbHelper;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        dbHelper = new NotificationDatabaseHelper(context);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -35,21 +47,36 @@ public class NotificationsFragment extends Fragment {
 
         // Initialize notification list
         notificationList = new ArrayList<>();
-        // Add sample notifications
-        notificationList.add(new Notification("Title 1", "Message 1", "Timestamp 1"));
-        notificationList.add(new Notification("Title 2", "Message 2", "Timestamp 2"));
-        notificationList.add(new Notification("Title 8", "Message 3", "Timestamp 3"));
+        fetchNotificationsFromDatabase();
 
         // Initialize adapter
         adapter = new NotificationAdapter(notificationList);
         recyclerView.setAdapter(adapter);
-
 
         // Hide the action bar
         if (getActivity() != null) {
             ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
         }
         return root;
+    }
+
+    private void fetchNotificationsFromDatabase() {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String selectQuery = "SELECT * FROM " + TABLE_NOTIFICATIONS;
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Notification notification = new Notification(cursor.getString(cursor.getColumnIndexOrThrow(NotificationDatabaseHelper.COLUMN_TITLE)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(NotificationDatabaseHelper.COLUMN_MESSAGE)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(NotificationDatabaseHelper.COLUMN_TIMESTAMP)));
+//                notification.setId(cursor.getInt(cursor.getColumnIndexOrThrow(NotificationDatabaseHelper.COLUMN_ID)));
+
+                Log.d("databse_status: ",notification.getTitle());
+                notificationList.add(notification);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
     }
 
     @Override
