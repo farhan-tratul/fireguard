f07f5d5b154bae6dfd024eea62ca709edcffc971
farhan <tratul.farhan@gmail.com>
2024-07-08 10:29:30 +0600

    Recycle view notification

---
+++ app/src/main/java/edu/ewubd/fireguard/ui/notifications/NotificationAdapter.java
@@ -0,0 +1,59 @@
+package edu.ewubd.fireguard.ui.notifications;
+
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+import edu.ewubd.fireguard.R;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+
+    private List<Notification> notificationList;
+
+    public NotificationAdapter(List<Notification> notificationList) {
+        this.notificationList = notificationList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_notification, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Notification notification = notificationList.get(position);
+        holder.title.setText(notification.getTitle());
+        holder.message.setText(notification.getMessage());
+        holder.timestamp.setText(notification.getTimestamp());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notificationList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title;
+        public TextView message;
+        public TextView timestamp;
+
+        public ViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.notification_title);
+            message = view.findViewById(R.id.notification_message);
+            timestamp = view.findViewById(R.id.notification_timestamp);
+        }
+    }
+}
+
+
